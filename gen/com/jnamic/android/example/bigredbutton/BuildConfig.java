e3e637df034202adb3c3dfe70fddda57d53567de
Stefan Hoth <sh@jnamic.com>
2013-04-05 22:11:01 +0200

    First commit

---
+++ gen/com/jnamic/android/example/bigredbutton/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jnamic.android.example.bigredbutton;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
